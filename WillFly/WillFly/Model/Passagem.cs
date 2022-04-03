d1ed6221a72f0e720f98337cc0e96f3ae1137269
Willian Proni <wifepro@hotmail.com>
2022-04-03 01:47:50 -0300

    Criação das Passagens

--- WillFly/WillFly/Model/Passagem.cs
+++ WillFly/WillFly/Model/Passagem.cs
@@ -5,7 +5,7 @@ namespace WillFly.Model
     public class Passagem
     {
         public int Id { get; set; }
-        /*public PrecoBase InfoCompra { get; set; } //Origem, Destino, Valor, Classe;*/
+        public PrecoBase Compra { get; set; }
         public Passageiro Passageiro { get; set; }
         public DateTime DataCadastro { get; set; }
     } 
