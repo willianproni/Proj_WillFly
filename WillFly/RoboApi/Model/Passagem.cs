911d80b31a61e1b633f9d459550bdace234ee305
Willian Proni <wifepro@hotmail.com>
2022-04-03 23:29:07 -0300

    Finalização Relatorio

---
+++ WillFly/RoboApi/Model/Passagem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RoboApi.Model
+{
+    public class Passagem
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("compra")]
+        public PrecoBase Compra { get; set; }
+        [JsonProperty("passageiro")]
+        public Passageiro Passageiro { get; set; }
+        [JsonProperty("datacadastro")]
+        public DateTime DataCadastro { get; set; }
+    }
+}
--- WillFly/RoboApi/Service/RelatorioApi.cs
+++ WillFly/RoboApi/Service/RelatorioApi.cs
@@ -21,7 +21,19 @@ namespace RoboApi.Service
                 HttpResponseMessage response = await BuscaApi.GetAsync("https://localhost:44367/api/PrecoBases");
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
-                File.WriteAllText(@"C:\5by5\WillFly\relatorio\relatorioPrecoBase.json", responseBody);
+                //File.WriteAllText(@"C:\5by5\WillFly\relatorio\relatorioPrecoBase.json", responseBody);
+
+                var precobase = JsonConvert.DeserializeObject<List<PrecoBase>>(responseBody);
+
+                var precoBaseLinq =
+                    (from dados in precobase
+                     orderby dados.DataInclusao descending
+                     select dados
+                    );
+
+                var precoBaseJson = JsonConvert.SerializeObject(precoBaseLinq, Formatting.Indented);
+                File.WriteAllText(@"C:\5by5\WillFly\relatorio\relatorioprecobasemes.json", precoBaseJson);
+
             }
             catch (Exception)
             {
@@ -30,15 +42,26 @@ namespace RoboApi.Service
             }
         }
 
-        public static async void ReadJsonPassagem()
+        public static async void ReadJsonPassagem(int mes)
         {
             try
             {
                 HttpResponseMessage response = await BuscaApi.GetAsync("https://localhost:44367/api/Passagems");
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
-                File.WriteAllText(@"C:\5by5\WillFly\relatorio\relatoriopassagens.json", responseBody);
+                //File.WriteAllText(@"C:\5by5\WillFly\relatorio\relatoriopassagens.json", responseBody);
+
+                var passagem = JsonConvert.DeserializeObject<List<Passagem>>(responseBody);
+
+                var passagemLinq = (
+                    from dados in passagem
+                    where dados.DataCadastro.Month.Equals(mes)
+                    select dados);
+
+                var passagemJson = JsonConvert.SerializeObject(passagemLinq, Formatting.Indented);
+                File.WriteAllText(@"C:\5by5\WillFly\relatorio\relatoriopassagens.json", passagemJson);
             }
+
             catch (Exception)
             {
 
